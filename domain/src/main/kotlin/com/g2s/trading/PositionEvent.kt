c321e1602cf60fa17928afe477d264e752811b59
LIG-JY <dldlsry053@naver.com>
2024-02-21 17:46:56 +0900

    temp6

--- domain/src/main/kotlin/com/g2s/trading/PositionEvent.kt
+++ domain/src/main/kotlin/com/g2s/trading/PositionEvent.kt
@@ -1,17 +1,12 @@
 package com.g2s.trading
 
 import com.g2s.trading.position.Position
-import com.g2s.trading.position.PositionRefreshData
 import org.springframework.context.ApplicationEvent
 
 sealed class PositionEvent(
     source: Any
 ) : ApplicationEvent(source) {
-    data class PositionOpenedEvent(
-        val source: Position
-    ) : PositionEvent(source)
-
-    data class PositionClosedEvent(
+    data class PositionSyncedEvent(
         val source: Position
     ) : PositionEvent(source)
 }
