6b14ef63512f8e64b59932d37f263621ced4b3d9
LIG-JY <dldlsry053@naver.com>
2024-02-23 01:41:20 +0900

    temp6

--- domain/src/main/kotlin/com/g2s/trading/NewSimpleCloseMan.kt
+++ domain/src/main/kotlin/com/g2s/trading/NewSimpleCloseMan.kt
@@ -73,7 +73,7 @@ class NewSimpleCloseMan(
 
     @EventListener
     fun handleMarkPriceEvent(event: TradingEvent.MarkPriceRefreshEvent) {
-        logger.debug("handleMarkPriceEvent: ${event.source.symbol}")
+//        logger.debug("handleMarkPriceEvent: ${event.source.symbol}")
         // find matching position
         val position = strategyPositionMap.asSequence()
             .map { it.value.second }
